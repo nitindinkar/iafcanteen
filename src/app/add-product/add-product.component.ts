681ca674c622427d1afeae16b36a0a23892a86bc
Sumer <sumersinha@gmail.com>
2024-03-18 14:24:21 +0530

    add to cart changes

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiCallingServiceService } from '../services/api-calling/api-calling-service.service';
 import { HttpClient } from '@angular/common/http';
@@ -9,13 +9,16 @@ import { ConstantsService } from '../services/constants/constants.service';
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.scss'
 })
-export class AddProductComponent {
+export class AddProductComponent implements OnInit {
   @ViewChild('invoiceFileInput') invoiceFileInput: any;
 
   name: any;
   desc: any;
   aPrice: any;
   dPrice: any;
+  categories: any;
+  category:any;
+  uploadId:any;
 
   constructor(
     private router: Router,
@@ -23,6 +26,30 @@ export class AddProductComponent {
     private apiService: ApiCallingServiceService,
     private cons: ConstantsService,
   ) {}
+    ngOnInit(): void {
+        this.getAllCategories();
+  }
+
+
+ 
+
+    getAllCategories() {
+      this.apiService.getApiWithToken(this.cons.api.getAllCategories).subscribe(
+        (response: object) => {
+          let result: { [key: string]: any } = response;
+           this.categories=result['response'];
+          
+          
+        },
+        (error) => {
+          console.error('Add Product failed:', error);
+        }
+      );
+    }
+
+
+
+  
 
 
   upload() {
@@ -46,21 +73,24 @@ export class AddProductComponent {
   }
 
 addProduct() {
-  const file: File = this.invoiceFileInput.nativeElement;
+  //const file: File = this.invoiceFileInput.nativeElement;
   // console.log(file);
   const formData = new FormData();
-  formData.append('imageFiles', file);
+  //formData.append('imageFiles', file);
   const jsonData = {
     productName: this.name,
     productDescription: this.desc,
     productActualPrice: this.aPrice,
-    productDiscountedPrice: this.dPrice
+    productDiscountedPrice: this.dPrice,
+    categoryId:this.category,
+    uploadId:this.upload
+    
 
   };
-  formData.append('json_data', JSON.stringify(jsonData));
-  debugger;
+  // formData.append('json_data', JSON.stringify(jsonData));
+  // debugger;
 
-  this.apiService.postApiWithToken(this.cons.api.addProduct, formData).subscribe({
+  this.apiService.postApiWithToken(this.cons.api.addProduct, jsonData).subscribe({
     next: (v: object) => {
       let result: { [key: string]: any } = v;
       if (result['message'] == 'success') {
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConstantsService } from '../services/constants/constants.service';
+import { ApiCallingServiceService } from '../services/api-calling/api-calling-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
-export class CartComponent {
+export class CartComponent implements OnInit{
+  products: any;
+  cart: any;
+  ngOnInit(): void {
+    this.getCartDetails();
+    
+  }
+
+
+  constructor(private cons:ConstantsService,
+    private apiService: ApiCallingServiceService,
+    private router: Router) {
+}
+
+  getCartDetails(){
+
+    this.apiService.getApiWithToken(this.cons.api.getCartDetailsOfUser).subscribe(
+      (response: object) => {
+        let result: { [key: string]: any } = response;
+        this.cart=result['response'];
+        debugger;
+        // for(let product of this.products){
+        //   product.imageUrl=this.cons.serviceUrl+product.imageUrl;
+
+        // }
+      },
+      (error) => {
+        console.error('Add Product failed:', error);
+      }
+    );
+  }
+
+  
+
+  
 
 }
